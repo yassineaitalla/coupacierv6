77124c630e4e65acd0b23a02fd5315d3194cbed9
yassineaitalla <m.aitallayassine@gmail.com>
2024-05-08 18:07:29 +0200

    Affichage de la page devis

--- src/Controller/AfficherDevisController.php
+++ src/Controller/AfficherDevisController.php
@@ -3,18 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Devis;
-use App\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AfficherDevisController extends AbstractController
 {
     #[Route('/devis', name: 'app_afficher_devis')]
-    public function afficherDevis(Devis $devis): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $devisRepository = $entityManager->getRepository(Devis::class);
+        $devis = $devisRepository->findAll();
+
         return $this->render('affichagedevis.html.twig', [
             'devis' => $devis,
         ]);
     }
 }
+
+    
+
+   
